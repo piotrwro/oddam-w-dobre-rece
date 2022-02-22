35bf10129455dc8595b2fffb2d975f83d0174c28
piotrwro <piotrwrobel76@interia.pl>
2022-02-22 17:58:02 +0100

    models created

--- oddam_w_dobre_rece/home/models.py
+++ oddam_w_dobre_rece/home/models.py
@@ -1,3 +1,37 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import RegexValidator
 from django.db import models
+TYPE = ((1, 'fundacja'),
+        (2, 'organizacja pozarządowa'),
+        (3, 'zbiórka lokalna'),
+        (4, 'domyślna fundacja'))
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    type = models.IntegerField(choices=TYPE)
+    categories = models.ManyToManyField(Category,)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category, )
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=255, help_text="ulica i numer domu")
+    phone_number = models.CharField(max_length=16, blank=True, null=True,
+                                    validators=[RegexValidator(regex=r"^\+?1?\d{8,15}$")])
+    city = models.CharField(max_length=64)
+    zip_code = models.CharField(max_length=8, help_text="XX-XXX")
+    pick_up_date = models.DateField()
+    pick_up_time = models.DateTimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(get_user_model(), null=True, on_delete=models.CASCADE)
+
+
+
+
+
--- oddam_w_dobre_rece/home/views.py
+++ oddam_w_dobre_rece/home/views.py
@@ -6,17 +6,22 @@ class Index(View):
     def get(self, request):
         return render(request, "index.html")
 
+
 class Form(View):
     def get(self, request):
         return render(request, "form.html")
+
 
 class FormConf (View):
     def get(self, request):
         return render(request, "form-confirmation.html")
 
+
 class Login(View):
     def get(self, request):
         return render(request, "login.html")
+
+
 class Register(View):
     def get(self, request):
-        return render(request,"register.html")
+        return render(request, "register.html")
